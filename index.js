eaf02303ea07055d282a5628edfd1c7e3c241430
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-19 14:56:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+function saturdayFun(target = 'roller-skate'){
+
+
+    return `This Saturday, I want to ${target}!`;
+   
+
+}
+saturdayFun();
+
+function mondayWork(target = 'go to the office'){
+
+    return `This Monday, I will ${target}.`
+
+}
+mondayWork();
+
+function wrapAdjective(string = "*"){
+
+    return function(parameter = "special"){
+        return `You are ${string}${parameter}${string}!`
+    }
+}
+
